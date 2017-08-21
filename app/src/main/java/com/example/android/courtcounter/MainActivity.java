1e690512849327fac6849f9a1fc390d8ce71ed34
Florence Bouvier <florencebres1@gmail.com>
2017-08-21 15:17:21 +0200

    counter commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.android.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A of 3 points.
+     */
+
+    public void plus3points(View v) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A of 2 points.
+     */
+
+    public void plus2points(View v) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A of 2 points.
+     */
+
+    public void plus1point(View v) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A .
+     */
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
